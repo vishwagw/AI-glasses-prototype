bd5b160e89faa91ef05787a41da5e6c763bc7788
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-24 08:18:16 +0530

    Update obj_detection_code1.py

--- obj_detection_code1.py
+++ obj_detection_code1.py
@@ -1,3 +1,7 @@
 # sample code for object detection:
 import cv2
 from picamera2 import Picamera2
+
+picam = Picamera2()
+picam.start()
+net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'model.caffemodel')
