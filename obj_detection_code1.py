bf9349bf9f6f54ece55ce6cf29d6a94735e77f06
Vishwa Randy GW <136871744+vishwagw@users.noreply.github.com>
2025-08-24 08:19:30 +0530

    Update obj_detection_code1.py

--- obj_detection_code1.py
+++ obj_detection_code1.py
@@ -2,6 +2,23 @@
 import cv2
 from picamera2 import Picamera2
 
+# Camera initialzation
 picam = Picamera2()
 picam.start()
+# detection dataset model
 net = cv2.dnn.readNetFromCaffe('deploy.prototxt', 'model.caffemodel')
+
+# while loop
+while True:
+    frame = picam.capture_array()
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+    net.setInput(blob)
+    detections = net.forward()
+    for i in range(detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
+            (startX, startY, endX, endY) = box.astype('int')
+            label = f'Object: {confidence*100:.2f}%'
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
+            cv2.putText(frame, label, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
